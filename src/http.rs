309e1f1fc7878dd1525cbcc77240097e702d7396
Eric McBride <eric.mcbride@toyotaconnected.com>
2018-12-10 17:39:11 -0600

    Base Reporting / Duration added (#29)
    * added error / success
    
    * added base timer for building purposes
    
    * Added timer
    
    * code reorg
    
    * correct timing

--- src/http.rs
+++ src/http.rs
@@ -1,33 +1,31 @@
-use reqwest;
+use reqwest::header::HeaderMap;
+use reqwest::{Client, Error, Response, Url};
 use std::sync::mpsc;
 use std::thread;
+use std::time::{Duration, Instant};
 
 pub struct HttpOptions {
     pub url: reqwest::Url,
     pub rps: u64,
     pub http_verb: String,
     pub duration: u64,
-    pub headers: reqwest::header::HeaderMap,
+    pub headers: HeaderMap,
     pub body: String,
 }
 
 // #TODO: See if we can get these 2 functions into an impl for HttpOptions.  Calling the 2
 // methods from load_driver seemd to be an issue, even when passing self as a mutable reference.
 fn post_request(
-    url: reqwest::Url,
-    headers: reqwest::header::HeaderMap,
+    client: Client,
+    url: Url,
+    headers: HeaderMap,
     body: String,
-) -> Result<reqwest::Response, reqwest::Error> {
-    let client = reqwest::Client::new();
+) -> Result<Response, Error> {
     let resp = client.post(url).headers(headers).json(&body).send()?;
     Ok(resp)
 }
-fn get_request(
-    url: reqwest::Url,
-    headers: reqwest::header::HeaderMap,
-    _: String,
-) -> Result<reqwest::Response, reqwest::Error> {
-    let client = reqwest::Client::new();
+
+fn get_request(client: Client, url: Url, headers: HeaderMap, _: String) -> Result<Response, Error> {
     let resp = client.get(url).headers(headers).send()?;
     Ok(resp)
 }
@@ -40,6 +38,7 @@ impl LoadDriver for HttpOptions {
     fn load_driver(&self) -> Result<(), Box<::std::error::Error>> {
         let (tx, rx) = mpsc::channel();
         let rps = self.rps.clone();
+        let client = Client::new();
 
         let resp = match self.http_verb.as_ref() {
             "POST" => post_request,
@@ -47,20 +46,51 @@ impl LoadDriver for HttpOptions {
             _ => get_request, // defaults
         };
 
-        for _ in 0..rps {
-            let tx = tx.clone();
-            let url = self.url.clone();
-            let headers = self.headers.clone();
-            let body = self.body.clone();
+        //#TODO Improve performance of this whole section and code quality and then add the
+        //infinite version of it for long running processes.  Maybe implement a limiter/token
+        //bucket to see if we can make the requests more spread out and less bursty
+        println!("Processing requests...");
+        let now = Instant::now();
+        let dur = Duration::new(self.duration, 0);
+        let mut count = 0;
+        let mut err_count = 0;
 
-            thread::spawn(move || tx.send(resp(url, headers, body)));
+        while now.elapsed() <= dur {
+            let execution_time = Instant::now();
+            for i in 0..rps {
+                let tx = tx.clone();
+                let url = self.url.clone();
+                let headers = self.headers.clone();
+                let body = self.body.clone();
+                let client = client.clone();
+
+                thread::spawn(move || tx.send(resp(client, url, headers, body)));
+
+                // If the requests are equal, and the time passed is less then a second we need to throttle
+                if i == (rps - 1) && now.elapsed() <= dur {
+                    let elapsed_time = Instant::now();
+                    let sleep_time =
+                        1000 as u32 - elapsed_time.duration_since(execution_time).subsec_millis();
+                    let sleep_ms = Duration::from_millis(sleep_time as u64);
+                    thread::sleep(sleep_ms);
+                }
+            }
+
+            for _ in 0..rps {
+                match rx.recv() {
+                    Ok(_) => count += 1,
+                    Err(_) => err_count += 1, // #TODO establish baselines for whats an error
+                }
+            }
         }
 
-        for _ in 0..rps {
-            // #TODO Decide what statistics we want to report
-            println!("Received: {:?}", rx.recv()?);
-        }
-
+        println!("success is {:?}", count);
+        println!("error count is {:?}", err_count);
         Ok(())
     }
+}
+
+pub struct LoadReport {
+    pub success_count: u64,
+    pub error_count: u64,
 }
